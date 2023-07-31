b8a18475396388bb4e5adf52b66a245fa638cf47
Jerônimo Lopes <lopes.jeronimo@gmail.com>
2023-07-31 11:51:08 -0300

    removendo metodos que nao serao utilizados na api

--- credit_card_app/views.py
+++ credit_card_app/views.py
@@ -7,6 +7,22 @@ from credit_card_app.serializers import CreditCardSerializer
 # Create your views here.
 
 
-class CreditCardViewSet(ModelViewSet):
+from rest_framework import mixins, viewsets
+
+
+class CreateListRetrieveViewSet(mixins.CreateModelMixin,
+                                mixins.ListModelMixin,
+                                mixins.RetrieveModelMixin,
+                                viewsets.GenericViewSet):
+    """
+    A viewset that provides `retrieve`, `create`, and `list` actions.
+
+    To use it, override the class and set the `.queryset` and
+    `.serializer_class` attributes.
+    """
+    pass
+
+
+class CreditCardViewSet(CreateListRetrieveViewSet):
     queryset = CreditCard.objects.all()
     serializer_class = CreditCardSerializer
